1e91cd06f3b811ff3837c200f5ed7e36e6758128
Maayan Shem <Maayanshem@gmail.com>
2016-05-26 01:15:09 +0300

    iNTER

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/test', function (req, res) {
     res.send('Hello World!');
 });
 
-app.use("/", express.static(path.join(__dirname, '/client')));
+
 
 app.get('/',function(req,res){
   res.sendfile('client/index.html');
@@ -33,6 +33,8 @@ app.get('/GetCurrPoint',function(req,res) {
   console.log("Requested point");
 res.send(JSON.stringify(currPoint));
 });
+
+app.use("/", express.static(path.join(__dirname, '/client')));
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
