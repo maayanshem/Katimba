f3513ac279ee368cffddb0d37f50bb94f2638e9c
Maayan Shem <Maayanshem@gmail.com>
2016-05-25 23:58:06 +0300

    Test ran

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ app.get('/test', function (req, res) {
     res.send('Hello World!');
 });
 
-
 app.post('/AddPoint', function (req, res) {
   console.log(req.body);
 });
 
-var obj = {'Name':'ID'};
-console.log(JSON.stringify(obj))
+app.get('/GetCurrPoint',function(req,res) {
+  console.log("Requested point");
+  res.send(JSON.stringify({a:1}));
+});
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
