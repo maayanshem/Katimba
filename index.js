f3e6876187da08136fd9c2a7783f1e39539c20e4
Maayan Shem <Maayanshem@gmail.com>
2016-05-26 01:18:26 +0300

    iNTER

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/GetCurrPoint',function(req,res) {
 res.send(JSON.stringify(currPoint));
 });
 
-app.use("/", express.static(path.join(__dirname, '/client')));
+app.use("/", express.static(path.join(__dirname, '/Client')));
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
